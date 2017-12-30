65f62edc4cf30a5549cc125b358d7b89437b6cd7
Dmitriy Bielorusov <d.belorusov@gmail.com>
2017-12-30 16:51:56 +0200

    Adaptae to new Patterms

--- lib/messaging/client.rb
+++ lib/messaging/client.rb
@@ -1,7 +1,20 @@
 require "messaging/client/version"
+require "messaging/client/mail"
 
 module Messaging
   module Client
-    # Your code goes here...
+    class << self
+      def logging_label
+        @logging_label ||= 'MESSAGING'
+      end
+
+      def url
+        @url ||= 'http://localhost:9293'
+      end
+
+      def api_token
+        @api_token ||= 'someSecretApiToken'
+      end
+    end
   end
 end
---
+++ lib/messaging/client/mail.rb
@@ -0,0 +1,56 @@
+module Messaging
+  module Client
+    class Mail
+      AVAILABLE_TYPES = %w[confirmation_pin]
+
+      attr_reader :type, :content, :emails, :api_token, :client
+
+      def initialize(opts = {})
+        @type = opts[:type]
+        @content = opts[:content]
+        @emails = opts[:emails]
+        @client = Curl::Easy.new
+        @client.url = "#{Messaging::Client.url}/mail_messages"
+      end
+
+      def valid?
+        return unless type&.in?(AVAILABLE_TYPES)
+        return unless emails
+        true
+      end
+
+      def send_mail
+        unless valid?
+          Logging.logger.error "[#{Messaging::Client.logging_label}] Invalid attributes"
+          return
+        end
+
+        begin
+          send(type)
+        rescue Curl::Err::CurlError => e
+          Logging.logger.error "[#{Messaging::Client.logging_label}] #{e.message} #{client.url}"
+        end
+      end
+
+      private
+        def apply_api_auth_headers
+          client.headers['Accept'] = 'application/json'
+          client.headers['Accept-Language'] = content[:locale] || 'en'
+          client.headers['X-API-TOKEN'] = Messaging::Client.api_token
+        end
+
+        def confirmation_pin
+          return unless pin =  @content[:pin]
+          fields = []
+          apply_api_auth_headers
+          emails.each do |email|
+            fields << Curl::PostField.content("message[emails][]", email.to_s)
+          end
+          fields << Curl::PostField.content("message[type]", type.to_s)
+          fields << Curl::PostField.content("message[content][pin]", pin.to_s)
+          client.http_post(*fields)
+          Logging.logger.error "[#{Messaging::Client.logging_label}] #{client.response_code} #{client.body_str}"
+        end
+    end
+  end
+end
